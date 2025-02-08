6f107757dc84bb616d004b5a7496d4b099f21d2d
me <2906805859@qq.com>
2025-02-08 17:58:34 +0800

    auth

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -23,7 +23,7 @@
     }
   
     try {
-      const response = await fetch('http://127.0.0.1:10005/api/auth/register', {
+      const response = await fetch('http://127.0.0.1:10004/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: username.value, password: password.value }),
